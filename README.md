808ea7e92c5d6757c3c8f1eed4de0a91f17ad32c
hpteach2021 <hooper.rob@gmail.com>
2021-09-21 16:34:30 +0100

    sorted double dice layout

---
+++ README.md
@@ -0,0 +1,8 @@
+# Translator 
+
+This is my translator from English to Morse and vice versa
+
+## Sub Title
+
+
+## Sub Sub Title
---
+++ script/main.js
@@ -0,0 +1,32 @@
+let images =[
+  "assets/dice-01.svg",
+  "assets/dice-02.svg",
+  "assets/dice-03.svg",
+  "assets/dice-04.svg",
+  "assets/dice-05.svg",
+  "assets/dice-06.svg"];
+
+let dice = document.querySelectorAll("img");
+
+const roll = () => {
+  dice.forEach(function(die) {
+    die.classList.add("shake");
+  });
+  setTimeout(function() {
+    dice.forEach(function(die) {
+      die.classList.remove("shake");
+    });
+    let dieOneValue = Math.floor((Math.random()*6))
+    ;
+    let dieTwoValue = Math.floor((Math.random()*6))
+    ;
+    document.querySelector("#die-1").setAttribute("src", images[dieOneValue]);
+    document.querySelector("#die-2").setAttribute("src", images[dieTwoValue]);
+    document.querySelector("#total").innerHTML = "Your roll is " + ((dieOneValue +1) + (dieTwoValue +1))
+  
+  },
+  1000
+  );
+}
+
+roll();
